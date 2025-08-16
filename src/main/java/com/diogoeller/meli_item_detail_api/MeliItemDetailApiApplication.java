a6dd83586a4257871698acda7d23476aec0112a2
DiogoEller <dbrunoeller@hotmail.com>
2025-08-16 13:18:52 -0300

    first commit

---
+++ src/main/java/com/diogoeller/meli_item_detail_api/MeliItemDetailApiApplication.java
@@ -0,0 +1,13 @@
+package com.diogoeller.meli_item_detail_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliItemDetailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeliItemDetailApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diogoeller/meli_item_detail_api/MeliItemDetailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogoeller.meli_item_detail_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliItemDetailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
